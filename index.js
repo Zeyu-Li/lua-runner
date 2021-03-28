8b84032e10d205f0ef126a2e97c9296d1ae62955
Zeyu-Li <zeyu.li23@gmail.com>
2021-03-28 10:03:22 -0600

    updated to new version

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ function run_lua_res(code, timeout) {
 }
 exports.run_lua_res = run_lua_res;
 // debug
-run_lua_res("    \n    function hello_lua()\n      print(\"Hello World!\")\n      return \"A\"\n    end\n    \n    return hello_lua()\n    ").then(function (e) {
-    console.log(e);
-});
+// run_lua_res("    \n    function hello_lua()\n      print(\"Hello World!\")\n      return \"A\"\n    end\n    \n    return hello_lua()\n    ").then(function (e) {
+//     console.log(e);
+// });
