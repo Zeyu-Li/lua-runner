fd0540b6bbd56a3f7664cf0cbe4de28c95366978
Zeyu-Li <zeyu.li23@gmail.com>
2021-03-28 09:52:58 -0600

    fixed index.d

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export { run_lua, run_lua_res } from "./index"
+import { run_lua, run_lua_res } from "./index"
 
 // response object
 interface response {
@@ -11,4 +11,4 @@ declare function run_lua(code: Array<string>, timeout: number): Promise<string>
 declare function run_lua_res(code: Array<string>, timeout: number): Promise<response>
 declare function run_lua_res(code: string, timeout: number): Promise<response>
 
-
+export { run_lua, run_lua_res }
