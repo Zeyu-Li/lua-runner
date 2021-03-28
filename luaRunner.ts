5fa9095b33458bbc276d0a90793c704ebe238384
Zeyu-Li <zeyu.li23@gmail.com>
2021-03-27 20:38:51 -0600

    update core

--- luaRunner.ts
+++ luaRunner.ts
@@ -1,9 +1,12 @@
+import { initWasmModule } from './webassem'
+
 /**
  * Run the lua code
  * @param code the code to be run in Lua
  * @param timeout an optional parameter that states the timeout of the code
  */
 export function run_lua(code: string, timeout = 5): void {
-    
+    let config
+    initWasmModule(config)
     return
 }
